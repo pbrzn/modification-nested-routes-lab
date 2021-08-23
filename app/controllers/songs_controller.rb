146aaf18d6e14a1a719f8eff53e33b6654f163f9
Patrick Brennan <pbrennanmusic@gmail.com>
2021-08-22 20:17:21 -0400

    commit tree d2a66ffbe3bd5f0e9011c4d043faf973aeee3291

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -39,7 +43,13 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    elsif params[:artist_id] && Artist.exists?(params[:artist_id]) && !Song.exists?(params[:id])
+      redirect_to artist_songs_path(params[:artist_id])
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +74,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
